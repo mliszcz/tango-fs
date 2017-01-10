63b5f7fc69efc9d4724238b7e5659a835f494eda
mliszcz <liszcz.michal@gmail.com>
2017-01-11 00:44:55 +0100

    boost::varint removal - step 1: getattr

--- include/handlers/getattr.hpp
+++ include/handlers/getattr.hpp
@@ -9,54 +9,13 @@
 
 namespace handlers {
 
-class getattr : boost::static_visitor<int> {
+using Stat = struct stat;
 
-private:
-
-    using Stat = struct stat;
-    Stat* stbuf;
-
-    template <typename Path>
-    void fillAsDirectory(const Path& path) const {
-        auto entries = lookup::directoryEntries(path)(tango::createDatabase,
-                                                      tango::createDeviceProxy);
-        Stat stat {};
-        stat.st_mode = S_IFDIR | 0755;
-        stat.st_nlink = 2 + entries.size();
-        *stbuf = stat;
-    }
-
-public:
-
-    getattr(Stat* stbuf)
-        : stbuf(stbuf) {}
-
-    int operator()(const paths::InvalidPath&) const {
-        return -ENOENT;
-    }
-
-    int operator()(const paths::DatabaseQueryPath& path) const {
-        fillAsDirectory(path);
-        return 0;
-    }
-
-    int operator()(const paths::DevicePath& path) const {
-        fillAsDirectory(path);
-        return 0;
-    }
-
-    int operator()(const paths::DeviceAttributesPath& path) const {
-        fillAsDirectory(path);
-        return 0;
-    }
-
-    int operator()(const paths::AttributePath& path) const {
-        fillAsDirectory(path);
-        return 0;
-    }
-
-    template <typename Path>
-    int operator()(const Path& path) const {
+template <typename Path>
+auto getattr(const Path& path) {
+    return [=](auto&& stbuf) {
+    // return [=](struct stat* stbuf) {
+        return [=](auto&& db_, auto&& dp_) {
 
         auto db = tango::createDatabase;
         auto dp = tango::createDeviceProxy;
@@ -70,7 +29,53 @@ public:
         stat.st_size = size.get_value_or(0);
         *stbuf = stat;
         return 0;
-    }
-};
+        };
+    };
+}
+
+
+template <typename Path>
+auto fillAsDirectory(const Path& path) {
+    // return [=](struct stat* stbuf) {
+    return [=](auto&& stbuf) {
+        return [=](auto&& db_, auto&& dp_) {
+
+        auto entries = lookup::directoryEntries(path)(tango::createDatabase,
+                                                      tango::createDeviceProxy);
+        Stat stat {};
+        stat.st_mode = S_IFDIR | 0755;
+        stat.st_nlink = 2 + entries.size();
+        *stbuf = stat;
+
+        return 0;
+        };
+    };
+}
+
+auto getattr(const paths::InvalidPath&) {
+// return [=](struct stat* stbuf) {
+    return [=](auto&& stbuf) {
+        return [=](auto&& db_, auto&& dp_) {
+
+        return -ENOENT;
+        };
+    };
+}
+
+auto getattr(const paths::DatabaseQueryPath& path) {
+    return fillAsDirectory(path);
+}
+
+auto getattr(const paths::DevicePath& path) {
+    return fillAsDirectory(path);
+}
+
+auto getattr(const paths::DeviceAttributesPath& path) {
+    return fillAsDirectory(path);
+}
+
+auto getattr(const paths::AttributePath& path) {
+    return fillAsDirectory(path);
+}
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,22 @@
 
 #include <filesystem.hpp>
 
+namespace {
+
+const auto handler = filesystem::makeFuseHandler(tango::createDatabase,
+                                                 tango::createDeviceProxy);
+
+constexpr auto getattr = [](auto&& p) {
+    return handlers::getattr(std::forward<decltype(p)>(p));
+};
+
+}
+
 int main(int argc, char *argv[])
 {
+
     struct fuse_operations ops{};
-    ops.getattr = &filesystem::getattr;
+    ops.getattr = [](auto... args) { return handler(getattr)(args...); };
     ops.readdir = &filesystem::readdir;
     ops.open = &filesystem::open;
     ops.read = &filesystem::read;
